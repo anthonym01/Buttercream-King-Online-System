c3a95684d34c68f6a4125238daeda4946e9bf9f2
anthonym01 <samuelmatheson15@gmail.com>
2025-04-19 21:44:59 -0500

    Refactor template test to read user input and log results; add new product upload test script

---
+++ tests/FG_UP01.js
@@ -0,0 +1,4 @@
+//product upload test
+const database = require('../modules/database_wrapper');
+const logs = require('../modules/logger');
+const readline = require('node:readline');
--- tests/template_test.js
+++ tests/template_test.js
@@ -1,18 +1,24 @@
 const database = require('../modules/database_wrapper');
 const logs = require('../modules/logger');
+const readline = require('node:readline');
 
 try {
     logs.info("Starting template test");
-    // Example of a test case
-    database.getCustomers().then((result) => {
-        logs.info("Result: ", result);
+
+    // Read username from console
+    const template_input = readline.createInterface({ input: process.stdin, output: process.stdout }); //emulate node console
+    template_input.question(`input example\n`, input => {
+        logs.info(`got input ${input}`);
+        template_input.close();// close the readline interface
+
+        //test passes at end of callback swamp
+        logs.info("--------------------------------------------------");
+        logs.info("--TEST PASSED-- :)");
     });
-    logs.info("--------------------------------------------------");
-    logs.info("--TEST PASSED-- :)");
+
 } catch (error) {
     logs.info("\n\n--------------------------------------------------");
     logs.error(error);
     logs.info("--------------------------------------------------");
-    logs.info("TEST FAILED!! :(");
-
+    logs.info("test FG01 FAILED!! :( due to runtime error");
 }
